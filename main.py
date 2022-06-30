dd7b71c4165418367086583116896d0c06e31b00
avinash-lokani <avinash.lokani@zemosolabs.com>
2022-06-30 10:20:00 +0530

    changes

--- main.py
+++ main.py
@@ -3,20 +3,10 @@ from service import ProductService
 
 
 def search(product_id):
-    for product in products:
-        if product.id == product_id:
-            return product
+    for obj in products:
+        if obj.id == product_id:
+            return obj
     return None
-
-
-def add():
-    product = search(int(input("Enter id of product to add suggestions")))
-    if product is not None:
-        suggested_id = int(input("Enter id of the product to add into suggestions"))
-        temp = ProductService.search(suggested_id)
-        ProductService.addSuggestions(product, temp)
-    else:
-        return
 
 
 products = []
@@ -34,7 +24,14 @@ ProductService.addSuggestions(computer, keyboard)
 ProductService.addSuggestions(computer, speaker)
 
 k = int(input("Enter 1 to add suggestions for a product, 2 to list products"))
-if k == 3:
-    add()
-elif k == 2:
-    ProductService.listProducts(products)
+while True:
+    if k == 1:
+        product = search(int(input("Enter id of product to add suggestions")))
+        if product is not None:
+            suggested_id = int(input("Enter id of the product to add into suggestions"))
+            temp = ProductService.search(suggested_id)
+            ProductService.addSuggestions(product, temp)
+    elif k == 2:
+        ProductService.listProducts(products)
+    else:
+        break
--- service/ProductService.py
+++ service/ProductService.py
@@ -5,6 +5,6 @@ def addSuggestions(product, suggestion):
 
 def listProducts(products):
     print("suggested Products for each product")
-    for i in products:
-        print(i)
+    for product in products:
+        print(product)
 
